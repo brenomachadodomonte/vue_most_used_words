e51404a9d4c893d48133393857caf80fdb955bc6
brenomachado <brenomachadodomonte@gmail.com>
2021-09-29 23:13:01 -0300

    Change window's title

--- src/background.js
+++ src/background.js
@@ -33,6 +33,12 @@ async function createWindow() {
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+
+  win.webContents.on('did-finish-load', () => {
+    console.log('teste');
+    const { title, version} = require('../package.json');
+    win.setTitle(`${title} :: ${version}`);
+  });
 }
 
 // Quit when all windows are closed.
