6c9bbeac4999eaff45b8088743c28657172a4e6e
brenomachado <brenomachadodomonte@gmail.com>
2021-09-30 08:52:43 -0300

    Creating Pill Component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>Home</div>
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import Pill from "./Pill";
 export default {
   name: 'Home',
+  components:  { Pill },
+  data: () => ({
+    groupedWords: [
+      {name: 'i', amount: 1234},
+      {name: 'you', amount: 900},
+      {name: 'he', amount: 853},
+    ]
+  }),
 }
 </script>
 
