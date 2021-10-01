ecaed0dea2eb484d6509d642b41f5e1a2976a2ee
brenomachado <brenomachadodomonte@gmail.com>
2021-10-01 09:09:30 -0300

    Creating File input

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,16 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+          multiple
+          chips
+          v-model="files"
+          append-outer-icon="mdi-send"
+          label="Select subtitles"
+          prepend-icon="mdi-message-text"
+          outlined
+          @click:append-outer="processSubtitles" />
+    </v-form>
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
     </div>
@@ -12,12 +23,18 @@ export default {
   name: 'Home',
   components:  { Pill },
   data: () => ({
+    files: [],
     groupedWords: [
       {name: 'i', amount: 1234},
       {name: 'you', amount: 900},
       {name: 'he', amount: 853},
     ]
   }),
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    }
+  }
 }
 </script>
 
