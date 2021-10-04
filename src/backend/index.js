8d222bc0b284112bd3b1e76035c2afddb3056b5a
brenomachado <brenomachadodomonte@gmail.com>
2021-10-04 14:51:22 -0300

    Getting files contents

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,18 @@
 const { ipcMain } = require('electron');
 
+const pathsToRow = require('./pathsToRow');
+
 ipcMain.on('process-subtitles', (event, paths) => {
     console.log(paths);
 
-    event.reply('process-subtitles', [
-        {name: 'i', amount: 1234},
-        {name: 'you', amount: 900},
-        {name: 'he', amount: 853},
-    ]);
+    pathsToRow(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply('process-subtitles', [
+                {name: 'i', amount: 1234},
+                {name: 'you', amount: 900},
+                {name: 'he', amount: 853},
+            ]);
+        })
+
 });
